7dd334aed9a43e512428e0f2912d0c0c3d21f424
vera-ab <verunja108@gmail.com>
2022-11-13 15:55:07 +0200

    Update prod config

--- settings/docker.py
+++ settings/docker.py
@@ -1,16 +1,19 @@
 from .base import *
+import os
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'studydb',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB', 'studydb'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '123'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
         'ATOMIC_REQUESTS': True
     }
 }
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kh%z9jz66va7zjb9w%qelhnfubcic^0zk(ie_#dk=72l+1r=rj')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media/'
--- settings/prod.py
+++ settings/prod.py
@@ -1,4 +1,9 @@
 from .docker import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*',]
+
+SECRET_KEY = 'my-super-secret-key'
+
+EMAIL_SERVER_USER = 'admin'
+EMAIL_SERVER_PASSOWRD = 'super_secret'
